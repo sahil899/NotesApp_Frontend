33e0a892b6ec32e6434282e6411cdc1ad6fdab1f
sahil899 <sahilsharma2471@gmail.com>
2022-04-06 10:30:18 +0530

    few more changes for heroku deployment

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.use(express.static(__dirname + "/dist/note-web-app"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/note-web-app/index.html"));
 });
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4200, () => {
+  console.log(`node app working on ${process.env.PORT}`);
+});
